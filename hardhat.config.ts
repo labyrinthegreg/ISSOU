cd0303891a1e0eedaa9de06465203651c4bc752e
LeoG <leo.gregori95@gmail.com>
2024-01-23 16:35:37 +0100

    Preparing config an deploy with simultaneous verify

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,22 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const config: HardhatUserConfig = {
+  networks: {
+    mumbai: {
+      url: process.env.MUMBAI_RPC,
+      chainId: 80001,
+      accounts: [process.env.PRIVATE_KEY!]
+    }
+  },
   solidity: "0.8.20",
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY
+  },
 };
 
 export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,24 +1,48 @@
-import { ethers } from "hardhat";
+import hre, { ethers } from "hardhat";
+import dotenv from 'dotenv'
+import { Ownable } from "../typechain-types";
+
+dotenv.config()
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  console.log('Deploying contracts with the account: ' + process.env.PUBLIC_KEY);
 
-  const lockedAmount = ethers.parseEther("0.001");
+  // Deploy Token
+  const RIZToken = await ethers.getContractFactory("RIZToken");
+  const rizToken = await RIZToken.deploy();
 
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  // Deploy Sale
+  const RisitasSale = await ethers.getContractFactory("RisitasSale");
+  const risitasSale = await RisitasSale.deploy(10, rizToken);
 
-  await lock.waitForDeployment();
+  await rizToken.waitForDeployment();
+  await risitasSale.waitForDeployment();
 
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  console.log("PolygonScan verification in progress...");
+  await verify(rizToken)
+  await verify(risitasSale)
+  console.log("PolygonScan verification done. ✅");
+  
+  console.log( "RIZToken: " + await rizToken.getAddress() );
+  console.log( "RisitasSale: " + await risitasSale.getAddress() );
+
 }
 
+const verify = async (contract: Ownable ) => {
+    console.log("Verifying contract...");
+    try {
+        await hre.run("verify:verify", {
+          address: await contract.getAddress(),
+          network: "mumbai"
+        });
+    } catch (e: any) {
+        if (e.message.toLowerCase().includes("already verified")) {
+            console.log("Already verified!");
+        } else {
+            console.log(e);
+        }
+    }
+};
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
