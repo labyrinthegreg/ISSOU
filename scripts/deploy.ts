d048a52695ab6420dcddb733f83e5f4fb154a09b
LeoG <leo.gregori95@gmail.com>
2024-01-26 13:32:22 +0100

    Adding deploy for vesting

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -11,21 +11,28 @@ async function main() {
   // Deploy Token
   const RIZToken = await ethers.getContractFactory("RIZToken");
   const rizToken = await RIZToken.deploy();
+
+  console.log("Deploying Vesting");
+  // Deploy Vesting
+  const RisitasVesting = await ethers.getContractFactory("RisitasVesting");
+  const risitasVesting = await RisitasVesting.deploy(rizToken);
   
   console.log('Deploying Sale');
   // Deploy Sale
   const RisitasSale = await ethers.getContractFactory("RisitasSale");
-  const risitasSale = await RisitasSale.deploy(rizToken);
+  const risitasSale = await RisitasSale.deploy(rizToken, await risitasVesting.getAddress(), risitasVesting);
 
   await rizToken.waitForDeployment();
   await risitasSale.waitForDeployment();
 
   console.log("PolygonScan verification in progress...");
   await verify(rizToken, [])
+  await verify(risitasVesting, [await rizToken.getAddress(), await risitasVesting.getAddress(), await risitasVesting.getAddress()])
   await verify(risitasSale, [await rizToken.getAddress()])
   console.log("PolygonScan verification done. ✅");
   
-  console.log( "RIZToken: " + await rizToken.getAddress() );
+  console.log("RIZToken: " + await rizToken.getAddress());
+  console.log( "RisitasVesting: " + await risitasVesting.getAddress() );
   console.log( "RisitasSale: " + await risitasSale.getAddress() );
 
 }
